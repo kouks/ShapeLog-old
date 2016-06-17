50f85687d677b16925610bfc284185357d02238f
Pavel Koch <kouks.koch@gmail.com>
2016-06-17 17:34:42 +0200

    Profile bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,23 @@
 @extends('layouts.master')
 
-@section('main')
-    
-    <div class="row">
+@section('main') 
+
+    @if(!isset($_SESSION["facebook_access_token"]))
+        <div class="row">
+            <div class="col-12 center area-4">
+                <h3>Our app requires you to login with Facebook</h3>
+            </div>
+
+            <div class="col-12 center area-4">
+                <a class="fb-button center" href="{{ $loginUrl }}">
+                    <i class="fa fa-2 fa-facebook"></i> &nbsp;&nbsp;
+                    Login with Facebook
+                </a>
+            </div>
+        </div>
+    @endif
+
+    <div class="row almost-white-bg">
         <div class="col-3">&nbsp;</div>
         <div class="col-3 area-4 right">
             <p class="right">
@@ -15,7 +30,7 @@
         <div class="col-2">&nbsp;</div>
     </div>
 
-    <div class="row almost-white-bg">
+    <div class="row">
         <div class="col-2">&nbsp;</div>
         <div class="col-4 area-4 right">
             <p class="right big">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi assumenda quibusdam sapiente, iste dolore necessitatibus saepe quae odit nam esse libero eum autem impedit temporibus! Hic nihil nesciunt ullam maxime fugit dignissimos adipisci aut ut ipsam vitae dolorem, unde labore eaque quia, dolores aliquid consequatur modi odit exercitationem delectus ratione.</p>
@@ -28,7 +43,7 @@
         <div class="col-3">&nbsp;</div>
     </div>
 
-    <div class="row">
+    <div class="row almost-white-bg">
         <div class="col-3">&nbsp;</div>
         <div class="col-3 area-4 right">
             <p class="right">
@@ -39,25 +54,6 @@
             <p class="left big">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi assumenda quibusdam sapiente, iste dolore necessitatibus saepe quae odit nam esse libero eum autem impedit temporibus! Hic nihil nesciunt ullam maxime fugit dignissimos adipisci aut ut ipsam vitae dolorem, unde labore eaque quia, dolores aliquid consequatur modi odit exercitationem delectus ratione.</p>
         </div>
         <div class="col-2">&nbsp;</div>
-    </div>
-
-    <div class="row almost-white-bg">
-        @if(!isset($_SESSION["facebook_access_token"]))
-            <div class="col-12 center area-4">
-                <h3>Our app requires you to login with Facebook</h3>
-        </div>
-
-            <div class="col-12 center area-4">
-                <a class="fb-button center" href="{{ $loginUrl }}">
-                    <i class="fa fa-2 fa-facebook"></i> &nbsp;&nbsp;
-                    Login with Facebook
-                </a>
-            </div>
-        @else
-            <div class="col-12 center area-4">
-                <h3>Logged in as {{ $user->first_name }}</h3>
-            </div>
-        @endif
     </div>
 
 @stop
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,15 @@
 
     <body id="master">
 
+        @if(isset($_SESSION["facebook_access_token"]))
+            <nav class="row top-bar">
+                    <a href="/profile" class="img"><img src="//graph.facebook.com/{{ $user->fbid }}/picture?type=large"></a>
+                    <a href="/profile" class="bold">Logged in as {{ $user->first_name }}</a>
+                    <a href="/profile">Go to profile</a>
+                    <a href="/profile/logout">Logout</a>
+            </nav>
+        @endif
+
         <div class="row almost-white-bg">
         	<div class="col-12 area-2 center">
         		<h1 class="theme-text">Shape Log</h1>
