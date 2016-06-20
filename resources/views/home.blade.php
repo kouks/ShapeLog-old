3458b01b81d37415bbd8f1ee0eca7cb1d6c6bca3
tomasptoszek <tomas.ptoszek@gmail.com>
2016-06-20 19:24:15 +0200

    Nadpisy odstavců, update loga

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,8 @@
             </p>
         </div>
         <div class="col-4 area-4 left">
-            <p class="left big">Welcome to Shape Log. Our main goal is to help you with tracking important information of your gym progress, to achieve the  best results!</p>
+            <h3 class="margin">About</h3>
+            <p class="left big">Welcome to Shape Log. Our main goal is to help you with tracking important information of your gym progress to achieve the best results!</p>
         </div>
         <div class="col-2">&nbsp;</div>
     </div>
@@ -19,6 +20,7 @@
     <div class="row">
         <div class="col-2">&nbsp;</div>
         <div class="col-4 area-4 right">
+            <h3 class="margin">Features</h3>
             <p class="right big">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi assumenda quibusdam sapiente, iste dolore necessitatibus saepe quae odit nam esse libero eum autem impedit temporibus! Hic nihil nesciunt ullam maxime fugit dignissimos adipisci aut ut ipsam vitae dolorem, unde labore eaque quia, dolores aliquid consequatur modi odit exercitationem delectus ratione.</p>
         </div>
         <div class="col-3 area-4 left">
