07c3ead25869100b23f40ef47784c677c61e2d57
Pavel Koch <kouks.koch@gmail.com>
2016-06-21 18:46:00 +0200

    typografické úpravy

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,15 @@
 <div class="row-90 footer">
     <div class="col-4 area-2">
-    	<p class="grey-text">
+    	<span class="grey-text">
     		Version 0.6 beta | <a href="/changelog.md">Changelog</a>
-    	</p>
-    	<p class="grey-text">
+    	</span>
+    	<span class="grey-text">
     		Created by <a href="http://sleanded.com">Sleanded Codes</a> &copy; | 2016
-    	</p>
+    	</span>
     </div>
 
     <div class="col-4 area-2">
-    	<p class="light-grey-text big">Menu</p>
+    	<span class="light-grey-text big">Menu</span>
     	<ul>
     		<li class="grey-text">FAQ</li>
             <li class="grey-text">About</li>
@@ -17,7 +17,7 @@
     </div>
 
     <div class="col-4 area-2">
-    	<p class="light-grey-text big">Links</p>
+    	<span class="light-grey-text big">Links</span>
         <ul>
             <a href="https://github.com/KouKs/ShapeLog"><li class="grey-text">GitHub</li></a>
             <li class="grey-text">Facebook</li>
