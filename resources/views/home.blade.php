ea36740a113d14a0083f994ca0f39b13998a029f
Pavel Koch <kouks.koch@gmail.com>
2016-06-26 22:02:22 +0200

    pročištění obrázků + typografie

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
         </div>
         <div class="col-4 area-4 left">
             <h5 class="margin">About</h5>
-            <p class="justify big">Welcome to Shape Log. Our main goal is to help you with tracking important information on your gym progress to achieve the best results! All you have to do is to login using your Facebook account. Don't worry, it's completely safe.</p>
+            <p class="justify big serif">Welcome to Shape Log. Our main goal is to help you with tracking important information on your gym progress to achieve the best results! All you have to do is to login using your Facebook account. Don't worry, it's completely safe.</p>
         </div>
         <div class="col-2">&nbsp;</div>
     </div>
@@ -21,11 +21,11 @@
         <div class="col-2">&nbsp;</div>
         <div class="col-4 area-4 right">
             <h5 class="margin">Features</h5>
-            <p class="justify big">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi assumenda quibusdam sapiente, iste dolore necessitatibus saepe quae odit nam esse libero eum autem impedit temporibus! Hic nihil nesciunt ullam maxime fugit dignissimos adipisci aut ut ipsam vitae dolorem.</p>
+            <p class="justify big serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi assumenda quibusdam sapiente, iste dolore necessitatibus saepe quae odit nam esse libero eum autem impedit temporibus! Hic nihil nesciunt ullam maxime fugit dignissimos adipisci aut ut ipsam vitae dolorem.</p>
         </div>
         <div class="col-3 area-4 left">
             <p class="left">
-                <img src="img/graf03.png">
+                <img src="img/graph.png">
             </p>
         </div>
         <div class="col-3">&nbsp;</div>
@@ -35,11 +35,11 @@
         <div class="col-3">&nbsp;</div>
         <div class="col-3 area-4 right">
             <p class="right">
-                <img src="img/medal.png">
+                <img src="img/seal.png">
             </p>
         </div>
         <div class="col-4 area-4 left">
-            <p class="justify big">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi assumenda quibusdam sapiente, iste dolore necessitatibus saepe quae odit nam esse libero eum autem impedit temporibus! Hic nihil nesciunt ullam maxime fugit dignissimos adipisci aut ut ipsam vitae dolorem.</p>
+            <p class="justify big serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi assumenda quibusdam sapiente, iste dolore necessitatibus saepe quae odit nam esse libero eum autem impedit temporibus! Hic nihil nesciunt ullam maxime fugit dignissimos adipisci aut ut ipsam vitae dolorem.</p>
         </div>
         <div class="col-2">&nbsp;</div>
     </div>
