74c8f098e4f62ec5c85ae4ea6d03c8b05fc9a0bd
Pavel Koch <kouks.koch@gmail.com>
2016-06-26 22:30:18 +0200

    texty

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
         <div class="col-2">&nbsp;</div>
         <div class="col-4 area-4 right">
             <h5 class="margin">Features</h5>
-            <p class="justify big serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi assumenda quibusdam sapiente, iste dolore necessitatibus saepe quae odit nam esse libero eum autem impedit temporibus! Hic nihil nesciunt ullam maxime fugit dignissimos adipisci aut ut ipsam vitae dolorem.</p>
+            <p class="justify big serif">Shape Log provides you the option to project the data that you have inserted into pretty and well-aranged graphs, which will help you with keeping track of your progress. Additionally, it is completely up to you what data you want to log by creating your own custom tags. You can also look up your friends and see how they do, as well as compare their progress.</p>
         </div>
         <div class="col-3 area-4 left">
             <p class="left">
@@ -39,7 +39,8 @@
             </p>
         </div>
         <div class="col-4 area-4 left">
-            <p class="justify big serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi assumenda quibusdam sapiente, iste dolore necessitatibus saepe quae odit nam esse libero eum autem impedit temporibus! Hic nihil nesciunt ullam maxime fugit dignissimos adipisci aut ut ipsam vitae dolorem.</p>
+            <h5 class="margin">Motivation</h5>
+            <p class="justify big serif">Our app is a great motivational tool, too! Since it is not pleasant to watch your stats decrease, we believe that it would be great factor contributing to your motivation. If that is not enough though, here is another fact - even your friends can check your graphs, and imagine them mocking at you as your progress goes down. Keep up the great work!</p>
         </div>
         <div class="col-2">&nbsp;</div>
     </div>
